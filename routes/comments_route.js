a4851053ab8d2dd606619be7ccdaa116f56c7ee4
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2024-11-25 22:34:29 +0200

    fix pr comments

--- routes/comments_route.js
+++ routes/comments_route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const commentController = require('../controllers/comments_controller');
 
 router.post('/', commentController.createComment);
+
 router.put('/comment/:id', commentController.updateComment);
 
 module.exports = router;
