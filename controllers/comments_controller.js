8733ca1e88599a570d23ef160de94623c1aec341
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2024-11-25 23:29:03 +0200

    add get for comments on soecific post

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -61,10 +61,27 @@ const deleteComment = async (req, res) => {
       res.status(400).send(error.message);
     }
   };
-  
 
+  
+const getAllCommentsForPost = async (req, res) => {
+    const postId = req.params.postId;
+  
+    try {
+      const comments = await CommentModel.find({ post: postId });
+  
+      if (comments.length === 0) {
+        return res.status(404).send("No comments found for this post");
+      }
+  
+      res.send(comments);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  };
+  
   module.exports = {
     createComment,
     updateComment,
-    deleteComment
+    deleteComment,
+    getAllCommentsForPost
   };
--- routes/comments_route.js
+++ routes/comments_route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const commentController = require('../controllers/comments_controller');
+const commentsController = require('../controllers/comments_controller');
 
-router.post('/', commentController.createComment);
+router.post('/', commentsController.createComment);
 
-router.put('/comment/:id', commentController.updateComment);
+router.put('/comment/:id', commentsController.updateComment);
 
-router.delete('/comment/:id', commentController.deleteComment);
+router.delete('/comment/:id', commentsController.deleteComment);
+
+router.get('/:postId', commentsController.getAllCommentsForPost);
 
 module.exports = router;
