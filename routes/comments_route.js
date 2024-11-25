1c431c25b38a777f298e438cbc15fe29c3c341d5
danacohen111 <danac2121@gmail.com>
2024-11-25 21:51:44 +0200

    fix import and export of comments and example request

--- routes/comments_route.js
+++ routes/comments_route.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const commentController = require('../controllers/commentController');
+const commentController = require('../controllers/comments_controller');
 
 router.post('/', commentController.createComment);
+
+module.exports = router;
