986756ddb3518f5e39d89b9e38326f8e7c75894d
danacohen111 <danac2121@gmail.com>
2024-11-25 20:42:32 +0200

    create post model, controller and route

--- routes/posts_route.js
+++ routes/posts_route.js
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/posts_controller");
 
+router.post("/", postsController.createPost);
+
 module.exports = router;
