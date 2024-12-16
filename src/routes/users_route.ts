1d2fd01dbfd461a0ace3f6320b802e6b975631ac
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2024-12-16 16:54:47 +0200

    fix delete route

--- src/routes/users_route.ts
+++ src/routes/users_route.ts
@@ -11,6 +11,6 @@ router.get("/:id", usersController.getById.bind(usersController));
 
 router.get("/", usersController.getAll.bind(usersController));
 
-router.delete("/", usersController.deleteItem.bind(usersController));
+router.delete("/:id", usersController.deleteItem.bind(usersController));
 
 export default router;
