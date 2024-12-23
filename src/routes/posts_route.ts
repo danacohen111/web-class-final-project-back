e7af8ce479d54883fb5de63b46bd49ed96496832
danacohen111 <danac2121@gmail.com>
2024-12-23 23:50:03 +0200

    add swagger setup and check it

--- src/routes/posts_route.ts
+++ src/routes/posts_route.ts
@@ -4,6 +4,41 @@ import { authMiddleware } from "../controllers/auth_controller";
 
 const router = express.Router();
 
+/**
+* @swagger
+* tags:
+*   name: Posts
+*   description: The Posts API
+*/
+/**
+* @swagger
+* /posts:
+*   get:
+*     summary: Get all posts
+*     description: Retrieve all posts
+*     tags: [Posts]
+*     responses:
+*       200:
+*         description: Posts retrieved successfully
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*                 properties:
+*                   title:
+*                     type: string
+*                   content:
+*                     type: string
+*                   sender:
+*                     type: string
+*                   _id:
+*                     type: string
+*       500:
+*         description: Internal server error
+*/
+
 router.get("/", postsController.getAll.bind(postsController));
 
 router.get("/:id", postsController.getById.bind(postsController));
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import postsRoute from "./routes/posts_route";
 import commentsRoute from "./routes/comments_route";
 import usersRoute from "./routes/users_route";
 import auth_routes from "./routes/auth_route";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +17,20 @@ app.use("/posts", postsRoute);
 app.use("/comments", commentsRoute);
 app.use("/users", usersRoute);
 app.use("/auth", auth_routes);
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "REST API",
+      version: "1.0.0",
+      description: "REST server including authentication using JWT",
+    },
+    servers: [{ url: "http://localhost:3000" }],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
