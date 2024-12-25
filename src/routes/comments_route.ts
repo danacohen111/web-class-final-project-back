656cd93c8120dd75fd6771eaecf35f115f9a74e8
danacohen111 <danac2121@gmail.com>
2024-12-25 21:35:22 +0200

    document comments apis

--- src/routes/comments_route.ts
+++ src/routes/comments_route.ts
@@ -4,13 +4,224 @@ import { authMiddleware } from "../controllers/auth_controller";
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * tags:
+ *   name: Comments
+ *   description: The Comments API
+ */
+
+/**
+ * @swagger
+ * /comments:
+ *   get:
+ *     summary: Retrieve a list of comments
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: query
+ *         name: post
+ *         schema:
+ *           type: string
+ *         description: The ID of the post to retrieve comments for
+ *     responses:
+ *       200:
+ *         description: A list of comments
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   post:
+ *                     type: string
+ *                   content:
+ *                     type: string
+ *                   sender:
+ *                     type: string
+ *                   _id:
+ *                     type: string
+ *       500:
+ *         description: Internal server error
+ */
+
 router.get("/", commentsController.getAll.bind(commentsController));
+
+/**
+ * @swagger
+ * /comments/{id}:
+ *   get:
+ *     summary: Get a comment by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The comment ID
+ *     responses:
+ *       200:
+ *         description: The comment description by ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 post:
+ *                   type: string
+ *                 content:
+ *                   type: string
+ *                 sender:
+ *                   type: string
+ *                 _id:
+ *                   type: string
+ *       404:
+ *         description: Comment not found
+ *       500:
+ *         description: Internal server error
+ */
 
 router.get("/:id", commentsController.getById.bind(commentsController));
 
+/**
+ * @swagger
+ * /comments:
+ *   post:
+ *     summary: Create a new comment
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               post:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *               sender:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: The created comment
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 post:
+ *                   type: string
+ *                 content:
+ *                   type: string
+ *                 sender:
+ *                   type: string
+ *                 _id:
+ *                   type: string
+ *       401:
+ *         description: Access denied
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post("/", authMiddleware, commentsController.create.bind(commentsController));
 
+/**
+ * @swagger
+ * /comments/{id}:
+ *   put:
+ *     summary: Update a comment by ID
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The comment ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               post:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *               sender:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: The updated comment
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 post:
+ *                   type: string
+ *                 content:
+ *                   type: string
+ *                 sender:
+ *                   type: string
+ *                 _id:
+ *                   type: string
+ *       401:
+ *         description: Access denied
+ *       404:
+ *         description: Comment not found
+ *       500:
+ *         description: Internal server error
+ */
+
 router.put("/:id", authMiddleware, commentsController.updateItem.bind(commentsController));
+
+/**
+ * @swagger
+ * /comments/{id}:
+ *   delete:
+ *     summary: Delete a comment by ID
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The comment ID
+ *     responses:
+ *       200:
+ *         description: The deleted comment
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 post:
+ *                   type: string
+ *                 content:
+ *                   type: string
+ *                 sender:
+ *                   type: string
+ *                 _id:
+ *                   type: string
+ *       401:
+ *         description: Access denied
+ *       404:
+ *         description: Comment not found
+ *       500:
+ *         description: Internal server error
+ */
 
 router.delete("/:id", authMiddleware, commentsController.deleteItem.bind(commentsController));
 
