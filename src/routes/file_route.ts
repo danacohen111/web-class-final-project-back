de2a9938ab210437b0405cbbcff795cfa7e8d7f2
danacohen111 <danac2121@gmail.com>
2025-03-12 18:59:35 +0200

    change base url

--- src/routes/file_route.ts
+++ src/routes/file_route.ts
@@ -4,7 +4,7 @@ import multer from "multer";
 const router = express.Router();
 
 // const base = "http://" + process.env.DOMAIN_BASE + ":" + process.env.PORT + "/";
-const base = "http://localhost:3000/";
+const base = process.env.BASE_URL;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
